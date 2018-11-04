a91dcee19853367dd9bcb0dab459bae4c93eee33
67tasman <league@WTS13.attlocal.net>
2018-11-04 12:56:59 -0800

    bahdh,kzdfliua
    zvfyliegwigfvb chxgvbiwareuetgfcba ewfgiudshibfLU SFgceI SULDFCGHB
    LkeufdgazhcgblIUGLIDiuslzfdbcairew tgfjkb
    sdlzfgaiuerfgkzlxiuedbglaisefgdisdhfgertnfb gflzisurbdksjdbf
    iudfhaisdoufh isegh diuhfudfh dfuhzslidfh fhuSlf fgliefbcvzl
    ufdaldsiucvhc

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -10,16 +10,22 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
-		//2. if they say no, 
+		String Monkey = JOptionPane.showInputDialog("do you like bannanas");
+		//2. if they say no,
+		if (Monkey.equals("no"))  {
+		JOptionPane.showMessageDialog(null, "You are Crazy!"); }
+		
 			//tell them they are crazy 
 			//and end quiz
 		//3. if they say yes
 		//	ask them what is their favorite hobby
+		if(Monkey.equals("yes")){
+		String Hobby = JOptionPane.showInputDialog("what is your favouite hobby?");
 		//	show a pop up that says "<your hobby> is much better with bananas!"
-
-		//4. OPTIONAL: if they say something other than “yes�? or “no�?
-		//	show a pop up that says “You are bananas!�?
+        	JOptionPane.showMessageDialog(null, Hobby +" is much better with bannanas!");}
+		//4. OPTIONAL: if they say something other than “yes�? or “no�?
+		//	show a pop up that says “You are bananas!�?
 	
 	}
+	}
 
-}
---
+++ src/extra/YearsAlive.java
@@ -0,0 +1,13 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class YearsAlive {
+	public static void main(String[] args) {
+int Years=2007;
+
+
+	}
+	}
+	
+
