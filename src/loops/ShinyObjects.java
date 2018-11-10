db675e8391b98c4b8085c4f7ef6992a3127f9089
PhoenixFeather101 <league@WTS-IM-17.attlocal.net>
2018-11-10 11:22:10 -0800

    mr. zee lov es shiny ob jets.

--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -8,16 +8,18 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
+	String SO = JOptionPane.showInputDialog(null, "howe many shiny objects do you want?");
+int
 		// 3. Play the sound that many times
-
+for(int i=0;i<4; i++) {
 		// 1. Call the method below
-
+		playMisterZee();
 	}
 
 	public static void playMisterZee() {
